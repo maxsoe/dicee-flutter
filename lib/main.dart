ee38baec1a379f476f02296a174b101f5e721ded
Max Soe <maxsoe@gmail.com>
2023-01-06 00:30:56 +0400

    7-62 Challenge 3 Solution

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,14 @@ class _DicePageState extends State<DicePage> {
   int dice1 = 2;
   int dice2 = 3;
 
+  void randomiseDice() {
+    setState(() {
+      dice1 = Random().nextInt(6) + 1;
+      dice2 = Random().nextInt(6) + 1;
+      debugPrint("Function called. D1 is $dice1, D2 is $dice2");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -36,13 +44,8 @@ class _DicePageState extends State<DicePage> {
               child: TextButton(
                 child: Image.asset('images/dice$dice1.png'),
                 onPressed: () {
-                  debugPrint(
-                      'Left die pressed. Before: D1 was $dice1, D2 was $dice2');
-                  setState(() {
-                    dice1 = Random().nextInt(6) + 1;
-                    dice2 = Random().nextInt(6) + 1;
-                    debugPrint("In D1 setState: D1 is $dice1, D2 is $dice2");
-                  });
+                  debugPrint('Left die pressed. D1 was $dice1, D2 was $dice2');
+                  randomiseDice();
                 },
               ),
             ),
@@ -51,13 +54,8 @@ class _DicePageState extends State<DicePage> {
               child: TextButton(
                 child: Image.asset('images/dice$dice2.png'),
                 onPressed: () {
-                  debugPrint(
-                      'Right die pressed. Before: D1 was $dice1, D2 was $dice2');
-                  setState(() {
-                    dice1 = Random().nextInt(6) + 1;
-                    dice2 = Random().nextInt(6) + 1;
-                    debugPrint("In D2 setState: D1 is $dice1, D2 is $dice2");
-                  });
+                  debugPrint('Right die pressed. D1 was $dice1, D2 was $dice2');
+                  randomiseDice();
                 },
               ),
             ),
